bc7b05f58cee69c5dbb9bba9877f08bcede205a1
akshitrawat123 <63223269+akshitrawat123@users.noreply.github.com>
2022-11-26 13:48:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+console.log(buttons)
+for(item of buttons){
+    item.addEventListener('click', (e)=>{
+        buttonText = e.target.innerText;
+        console.log(buttonText)
+        if(buttonText=='*'){
+            buttonText = "*";
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if(buttonText == 'C'){
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if(buttonText == '='){
+            screen.value = eval(screenValue); 
+        }
+        else{
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
+}
+
+
+
